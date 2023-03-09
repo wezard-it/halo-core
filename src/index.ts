c1323f473890f01d0cedc955b6720b3d1c34be51
rolud <rolud.dev@gmail.com>
2023-03-09 15:07:40 +0100

    feat: type utils

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import * as Types from './types'
 
 export * from './core'
+export * from './agent'
+export * from './room'
 export * from './user'
+export * from './utils'
 
 export { Types }
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import type { HaloModule } from './core'
+import type { Agent, AgentDetails, User, UserDetails } from './types'
 
 export const loadModule = async (module: HaloModule) => {
   switch (module) {
@@ -8,5 +9,24 @@ export const loadModule = async (module: HaloModule) => {
       return await import('@wezard/react-native-halo-firebase')
     default:
       throw new Error('Module unavailable')
+  }
+}
+
+export const userToUserDetails = (user: User): UserDetails => {
+  return {
+    id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    image: user.image,
+    nickname: user.nickname,
+  }
+}
+
+export const agentToAgentDetails = (agent: Agent): AgentDetails => {
+  return {
+    id: agent.id,
+    firstName: agent.firstName,
+    lastName: agent.lastName,
+    image: agent.image,
   }
 }
